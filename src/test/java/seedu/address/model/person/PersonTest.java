c23a52adb12f19dfbcb36abc2e0ad90fa6c9de08
e0543403 <e0543403@u.nus.edu>
2021-10-23 19:21:24 +0800

    Add test cases for duplicate detection

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -2,7 +2,10 @@ package seedu.address.model.person;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DETAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_GITHUB_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_LINKEDIN_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
@@ -30,23 +33,57 @@ public class PersonTest {
         // null -> returns false
         assertFalse(ALICE.isSamePerson(null));
 
-        // same name, all other attributes different -> returns true
+        // same name, all other attributes different -> returns false
         Person editedAlice = new PersonBuilder(ALICE).withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB)
-                .withTags(VALID_TAG_HUSBAND).build();
+                .withTags(VALID_TAG_HUSBAND).withGithub(VALID_GITHUB_AMY).withLinkedIn(VALID_LINKEDIN_AMY)
+                .withDetail(VALID_DETAIL_BOB).build();
+        assertFalse(ALICE.isSamePerson(editedAlice));
+
+        // same name, all other attributes different except LinkedIn -> returns true
+        editedAlice = new PersonBuilder(ALICE).withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB)
+                .withTags(VALID_TAG_HUSBAND).withGithub(VALID_GITHUB_AMY).withDetail(VALID_DETAIL_BOB).build();
         assertTrue(ALICE.isSamePerson(editedAlice));
+
+        // same name, all other attributes different except Github -> returns true
+        editedAlice = new PersonBuilder(ALICE).withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB)
+                .withTags(VALID_TAG_HUSBAND).withLinkedIn(VALID_LINKEDIN_AMY).withDetail(VALID_DETAIL_BOB).build();
+        assertTrue(ALICE.isSamePerson(editedAlice));
+
+        // same name, all other attributes different except Email -> returns true
+        editedAlice = new PersonBuilder(ALICE).withPhone(VALID_PHONE_BOB).withTags(VALID_TAG_HUSBAND)
+                .withGithub(VALID_GITHUB_AMY).withLinkedIn(VALID_LINKEDIN_AMY).withDetail(VALID_DETAIL_BOB).build();
+        assertTrue(ALICE.isSamePerson(editedAlice));
+
+        // same name, all other attributes different except Phone -> returns true
+        editedAlice = new PersonBuilder(ALICE).withEmail(VALID_EMAIL_BOB).withTags(VALID_TAG_HUSBAND)
+                .withGithub(VALID_GITHUB_AMY).withLinkedIn(VALID_LINKEDIN_AMY).withDetail(VALID_DETAIL_BOB).build();
+        assertTrue(ALICE.isSamePerson(editedAlice));
+
+        // same name, all other attributes empty -> returns false
+        editedAlice = new PersonBuilder(ALICE).withEmptyEmail().withEmptyPhone()
+                .withEmptyGithub().withEmptyLinkedin().build();
+        assertFalse(ALICE.isSamePerson(editedAlice));
+        assertFalse(editedAlice.isSamePerson(ALICE));
+
+        // same name, both of all attributes empty -> returns true
+        Person emptyAlice = new PersonBuilder(ALICE).withEmptyEmail().withEmptyPhone()
+                .withEmptyGithub().withEmptyLinkedin().build();
+        assertTrue(emptyAlice.isSamePerson(editedAlice));
+        assertTrue(editedAlice.isSamePerson(emptyAlice));
+
 
         // different name, all other attributes same -> returns false
         editedAlice = new PersonBuilder(ALICE).withName(VALID_NAME_BOB).build();
         assertFalse(ALICE.isSamePerson(editedAlice));
 
-        // name differs in case, all other attributes same -> returns false
+        // name differs in case, all other attributes same -> returns true
         Person editedBob = new PersonBuilder(BOB).withName(VALID_NAME_BOB.toLowerCase()).build();
-        assertFalse(BOB.isSamePerson(editedBob));
+        assertTrue(BOB.isSamePerson(editedBob));
 
-        // name has trailing spaces, all other attributes same -> returns false
+        // name has trailing spaces, all other attributes same -> returns true
         String nameWithTrailingSpaces = VALID_NAME_BOB + " ";
         editedBob = new PersonBuilder(BOB).withName(nameWithTrailingSpaces).build();
-        assertFalse(BOB.isSamePerson(editedBob));
+        assertTrue(BOB.isSamePerson(editedBob));
     }
 
     @Test
