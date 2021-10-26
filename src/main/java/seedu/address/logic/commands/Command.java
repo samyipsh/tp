608907462ee7477c373801df2ebf44339627a8d1
DystoriaX <dascogabriel@gmail.com>
2021-10-26 08:05:47 +0800

    Create AliasCommand

--- src/main/java/seedu/address/logic/commands/Command.java
+++ src/main/java/seedu/address/logic/commands/Command.java
@@ -3,10 +3,30 @@ package seedu.address.logic.commands;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
+import java.util.List;
+
 /**
  * Represents a command with hidden internal logic and the ability to be executed.
  */
 public abstract class Command {
+
+    private static final List<String> COMMAND_WORDS = List.of(
+            AddCommand.COMMAND_WORD,
+            AliasCommand.COMMAND_WORD,
+            ClearCommand.COMMAND_WORD,
+            DeleteCommand.COMMAND_WORD,
+            EditCommand.COMMAND_WORD,
+            ExitCommand.COMMAND_WORD,
+            FindCommand.COMMAND_WORD,
+            HelpCommand.COMMAND_WORD,
+            ListCommand.COMMAND_WORD,
+            ReplaceTagCommand.COMMAND_WORD,
+            ShowTagsCommand.COMMAND_WORD,
+            TagAllCommand.COMMAND_WORD,
+            TagCommand.COMMAND_WORD,
+            UntagAllCommand.COMMAND_WORD,
+            UntagCommand.COMMAND_WORD
+    );
 
     /**
      * Executes the command and returns the result message.
@@ -17,4 +37,7 @@ public abstract class Command {
      */
     public abstract CommandResult execute(Model model) throws CommandException;
 
+    public static boolean isExistingCommand(String commandWord) {
+        return COMMAND_WORDS.stream().anyMatch(existingCommandWord -> existingCommandWord.equals(commandWord));
+    }
 }
