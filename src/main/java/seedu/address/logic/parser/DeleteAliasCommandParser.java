c12eccc2e782f770c93e2da6531142cbe5bb142a
e0543403 <e0543403@u.nus.edu>
2021-10-28 22:43:56 +0800

    Fix bugs relating to DeleteAlias command

--- src/main/java/seedu/address/logic/parser/DeleteAliasCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteAliasCommandParser.java
@@ -1,30 +1,24 @@
 package seedu.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.logic.commands.DeleteAliasCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 public class DeleteAliasCommandParser implements Parser<DeleteAliasCommand> {
-    public static final String MESSAGE_INVALID_COMMAND_ALIAS = "%1$s is not an existing command word";
-
-    public static final String MESSAGE_INVALID_ALIAS = "%1$s is an existing command word. "
-            + "It cannot be used as an alias";
 
     @Override
     public DeleteAliasCommand parse(String userInput) throws ParseException {
-        String[] arguments = userInput.trim().split("\\s+", 1);
+        requireNonNull(userInput);
 
-        if (arguments.length != 1) {
+        String args = userInput.trim();
+
+        if (args.equals("")) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteAliasCommand.MESSAGE_USAGE));
         }
 
-        String alias = parseAlias(arguments);
-        return new DeleteAliasCommand(alias);
+        return new DeleteAliasCommand(args);
     }
 
-    private String parseAlias(String[] args) {
-        assert args.length == 1;
-        return args[0];
-    }
 }
