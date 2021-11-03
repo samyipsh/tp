5717a1cec4297dcab658e27fd4e6606401f03aef
DystoriaX <dascogabriel@gmail.com>
2021-11-03 22:03:59 +0800

    Fix alias bug
    A bug was reported that prefix of existing command should show error
    message, despite of the warning given in the user guide. Another bug
    reported is that alias can contain existing command word.
    
    Alias replacement is done if it matches a word instead of substring.
    Additional check is created when parsing addalias command.
    
    It helps to create a more robust system.
    
    Resolves: #151, #142

--- src/main/java/seedu/address/logic/commands/Command.java
+++ src/main/java/seedu/address/logic/commands/Command.java
@@ -42,7 +42,17 @@ public abstract class Command {
         return List.copyOf(COMMAND_WORDS);
     }
 
-    public static boolean isExistingCommand(String commandWord) {
-        return COMMAND_WORDS.stream().anyMatch(existingCommandWord -> existingCommandWord.equals(commandWord));
+    /**
+     * Checks whether the given string is an existing command word.
+     */
+    public static boolean isExistingCommand(String s) {
+        return COMMAND_WORDS.stream().anyMatch(existingCommandWord -> s.equals(existingCommandWord));
+    }
+
+    /**
+     * Checks whether the the given string contains an existing command word.
+     */
+    public static boolean isContainingExistingCommand(String s) {
+        return COMMAND_WORDS.stream().anyMatch(existingCommandWord -> s.contains(existingCommandWord));
     }
 }
